a4e333b6cb5f505f22d16e7f44b281c1bc7a37dd
Michael <m.lauer@gmx.de>
2020-07-05 17:08:36 +0200

    Add files via upload

--- samples/common/v8set.js
+++ samples/common/v8set.js
@@ -1,11 +1,18 @@
+
+function checkForDuplicates(array) { 
+  return new Set(array).size !== array.length 
+}
+
 function main() { 
   
   let originalArray = [1, 2, 3, 4, 1, 2, 3, 4]  
   let uniqueArray = Array.from(new Set(originalArray)) // uniqueArray = [1, 2, 3, 4]
 
   console.log(originalArray)
+  console.log(checkForDuplicates(originalArray))
   
   console.log(uniqueArray)
+  console.log(checkForDuplicates(uniqueArray))
 
   return ""; 
 };
